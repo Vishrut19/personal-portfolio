3872f8dcc834ded0c4e07a0ecab028d3e4370b32
Vishrut19 <vishrutagarwalla19@gmail.com>
2022-11-07 10:46:34 +0530

    First Commit

---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const projects = [
+  {
+    title: "React Pokemon App",
+    subtitle: "ReactJS App and PokeAPI",
+    description:
+      "This is a Pokemon App built using ReactJS and PokeAPI, PokeAPI contains database of almost of the pokemons",
+    image: "./Pokemon App.png",
+    link: "https://github.com/Vishrut19/Pokemon-App",
+  },
+  {
+    title: "React Password Generator App",
+    subtitle: "ReactJS App",
+    description:
+      "ReactJS Password Generator App which can help you to generate safe and secure passwords for all your social handles",
+    image: "./Password Generator.png",
+    link: "https://frolicking-semolina-b73723.netlify.app",
+  },
+  {
+    title: "Personal Portfolio App using ReactJS",
+    subtitle: "React and EmailJS",
+    description:
+      "Created a Dummy Portfolio App using ReactJS and Contact Form using EmailJS for real-time email sending and enquiry form",
+    image: "./Dummy Profile.png",
+    link: "https://vishrut19-friendly-archimedes-1b2ed4.netlify.app/",
+  },
+  // {
+  //   title: "Epic Todo App",
+  //   subtitle: "React Hooks",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  //   image: "./project-4.gif",
+  //   link: "https://reactbootcamp.com",
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "GitHub",
+  "Python",
+  "Java",
+  "PHP",
+  "Django",
+  "MySQL",
+  "TypeScript",
+  "HTML and CSS",
+  "Bootstrap"
+];
